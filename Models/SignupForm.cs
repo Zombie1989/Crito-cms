673928a35639e4b1deeb63b1f994d2dc4752d1e3
Daniel Grahn <daniel_grahn@live.se>
2023-10-03 14:24:59 +0200

    Made some small changes

---
+++ Models/SignupForm.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crito.Models
+{
+    public class SignupForm
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
+        public string Email { get; set; } = null!;
+    }
+}
